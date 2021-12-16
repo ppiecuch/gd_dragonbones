354794da44d0f7366718e94494f825ae21cab994
Pawel Piecuch <pawel.piecuch@roche.com>
2021-12-16 17:00:26 +0100

    cleanup build options

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+
 def can_build(env, platform):
-    return platform != "android" or env["android_rtti"]
+    return (
+        not env["production"]
+        and (platform != "android" or env["android_rtti"])
+        and (platform != "iphone" or env["ios_exceptions"])
+    )
 
 
 def configure(env):
